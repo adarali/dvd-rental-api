143a2ab667d9a8e1b4a6cbe55d80ffc80e15e648
zeus <adarali.tpp@gmail.com>
2020-12-03 12:57:26 -0600

    Return 401 instead instead of redirecting to login page

--- src/main/java/com/example/rest/dvdrental/v2/config/security/WebSecurityConfig.java
+++ src/main/java/com/example/rest/dvdrental/v2/config/security/WebSecurityConfig.java
@@ -5,10 +5,14 @@ import com.example.rest.dvdrental.v2.utils.AppUtils;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import java.util.Arrays;
 
@@ -34,6 +38,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest().authenticated();
         
         http.oauth2Login().successHandler(authenticationSuccessHandler);
+        http.exceptionHandling().defaultAuthenticationEntryPointFor(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED), new AntPathRequestMatcher("/api/**"));
         http.oauth2ResourceServer().jwt().jwtAuthenticationConverter(new AppJwtAuthenticationConverter());
         
         //to be able to view h2 console
