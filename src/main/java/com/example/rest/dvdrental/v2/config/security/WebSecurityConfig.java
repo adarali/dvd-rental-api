4a8e22215da6535acd478c9e63940453197d6b5d
zeus <adarali.tpp@gmail.com>
2020-12-03 13:08:33 -0600

    removed unused imports

--- src/main/java/com/example/rest/dvdrental/v2/config/security/WebSecurityConfig.java
+++ src/main/java/com/example/rest/dvdrental/v2/config/security/WebSecurityConfig.java
@@ -10,7 +10,6 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
