73c9ba9b77afe5b7fb5f39f660ebfaf5f576bd56
zeus <adarali.tpp@gmail.com>
2020-12-23 23:11:36 -0600

    fixed react front-end movies menu item

--- src/main/java/com/example/rest/dvdrental/v2/config/WebMvcConfig.java
+++ src/main/java/com/example/rest/dvdrental/v2/config/WebMvcConfig.java
@@ -1,6 +1,7 @@
 package com.example.rest.dvdrental.v2.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -12,6 +13,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
         String redirectUrl = "/app/";
         registry.addViewController("/app/").setViewName("forward:/app/index.html");
         registry.addViewController("/react/").setViewName("forward:/react/index.html");
+//        registry.addViewController("/react/index.html").setStatusCode(HttpStatus.OK);
         registry.addRedirectViewController("/", redirectUrl);
         registry.addRedirectViewController("/app", redirectUrl);
         registry.addRedirectViewController("/react", "/react/");
